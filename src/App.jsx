70bbe7679361031c08de600010b75aa070770ff1
Yinna Gomez <yinngomz186@gmail.com>
2025-04-23 20:55:05 -0500

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
-import Navbar from './Components/Navbar/Navbar'
+import NavBar from './Components/NavBar/NavBar'
 import HomePage from './Pages/HomePage/HomePage'
 import DetailsPage from './Pages/DetailsPage/DetailsPage'
 import ErrorPage from './Pages/ErrorPage/ErrorPage'
@@ -17,7 +17,7 @@ const App = () => {
     <>
       <Header />
       <BrowserRouter>
-        <Navbar />
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/filter/:genero" element={<FilterPage />} />
